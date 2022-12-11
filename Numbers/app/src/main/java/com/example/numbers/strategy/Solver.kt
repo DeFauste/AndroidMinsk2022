225311b34e092b40cbbcf8c78bba6b9500cadcd0
DeFauste <39949940+DeFauste@users.noreply.github.com>
2022-12-11 23:02:39 +0300

    add: class Solver Strategy pattern

---
+++ Numbers/app/src/main/java/com/example/numbers/strategy/Solver.kt
@@ -0,0 +1,33 @@
+package com.example.numbers.strategy
+
+interface Solver {
+    fun getResult(numbers: ArrayList<Int>): Int
+}
+
+class SumListInt : Solver {
+    override fun getResult(numbers: ArrayList<Int>): Int {
+        return numbers.sum()
+    }
+}
+
+class AverageListInt : Solver {
+    override fun getResult(numbers: ArrayList<Int>): Int {
+        return numbers.average().toInt()
+    }
+}
+
+class MixedListInt : Solver {
+    override fun getResult(numbers: ArrayList<Int>): Int {
+        val sum = numbers.sum()
+        val difference = difference(numbers)
+        return sum / difference
+    }
+
+    private fun difference(numbers: ArrayList<Int>): Int {
+        var result = 0
+        numbers.forEach {
+            result -= it
+        }
+        return result
+    }
+}
