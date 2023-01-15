8e554573c748a26a0bc4c865fefca30c78a1dfcf
DeFauste <39949940+DeFauste@users.noreply.github.com>
2023-01-15 16:07:37 +0300

    add: navigation graph

--- Weather/app/src/main/java/com/example/weather/MainActivity.kt
+++ Weather/app/src/main/java/com/example/weather/MainActivity.kt
@@ -15,24 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lifecycleScope.launchWhenCreated {
-            val response = try {
-                RetrofitInstance.api.getWeather("London","ru","metric")
-            }catch (e: IOException) {
-                println( "onCreate: not internet")
-                return@launchWhenCreated
-            }catch (e: HttpException) {
-                println( "HttpException")
-                return@launchWhenCreated
-            }
-            if(response.isSuccessful && response.body() != null) {
-//                findViewById<TextView>(R.id.text_main).text = response.body()?.list?.get(0)?.main?.temp.toString()
-                println(response.body().toString())
-            }
-        }
     }
+
 }
 
 
 
 
+//        lifecycleScope.launchWhenCreated {
+//            val response = try {
+//                RetrofitInstance.api.getWeather("London","ru","metric")
+//            }catch (e: IOException) {
+//                println( "onCreate: not internet")
+//                return@launchWhenCreated
+//            }catch (e: HttpException) {
+//                println( "HttpException")
+//                return@launchWhenCreated
+//            }
+//            if(response.isSuccessful && response.body() != null) {
+////                findViewById<TextView>(R.id.text_main).text = response.body()?.list?.get(0)?.main?.temp.toString()
+//                println(response.body().toString())
+//            }
+//        }
